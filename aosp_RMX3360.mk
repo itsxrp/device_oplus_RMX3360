d9879cef3ceb81905b4cdd747749e795a6ce5421
itsxrp <itsxrproms@gmail.com>
2022-07-09 13:49:51 +0200

    Bringup Project Elixir
    Signed-off-by: itsxrp <itsxrproms@gmail.com>

--- aosp_RMX3360.mk
+++ aosp_RMX3360.mk
@@ -1,9 +1,3 @@
-#
-# Copyright (C) 2021-2022 The LineageOS Project
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
@@ -11,8 +5,22 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from RMX3360 device
 $(call inherit-product, device/oplus/RMX3360/device.mk)
 
-# Inherit some common Lineage stuff.
+# Inherit some common Elixir Stuff
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+
+# Official-ify
+Elixir_MAINTAINER := X-R-P
+IS_PHONE := true
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_SUPPORTS_CALL_RECORDING := true
+CUSTOM_BUILD_TYPE := OFFICIAL
+TARGET_SUPPORTS_GOOGLE_RECORDER := false
+TARGET_INCLUDE_STOCK_ACORE := false
+TARGET_INCLUDE_LIVE_WALLPAPERS := false
+EXTRA_UDFPS_ANIMATIONS := true
+
+#Bootanimation 
+TARGET_BOOT_ANIMATION_RES := 1080
 
 PRODUCT_NAME := aosp_RMX3360
 PRODUCT_DEVICE := RMX3360
